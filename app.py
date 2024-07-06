20bfa7a9b49b322e14a59d9ea21d32aa1e4f7c0f
AviLPA <101359358+AviLPA@users.noreply.github.com>
2024-07-06 12:27:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify, render_template, Response, send_from_directory
 import os
 import cv2
-from PIL import Image, UnidentifiedImageError
+import numpy as np
 import hashlib
 import requests
 from blockfrost import BlockFrostApi
 import logging
 import time
-import numpy as np
+from PIL import Image, UnidentifiedImageError
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
@@ -20,57 +20,8 @@ api = BlockFrostApi(project_id=API_KEY)
 
 progress_data = {'processed_frames': 0, 'total_frames': 0}
 
-def image_to_binary(image, num_colors=8):
-    try:
-        logging.debug("Converting image to binary")
-        quantized_image = image.convert("L").quantize(colors=num_colors)
-        width, height = quantized_image.size
-        binary_code = ""
-        palette = quantized_image.getpalette()
-        color_to_binary = {}
-        bits_needed = len(bin(num_colors - 1)[2:])
-        for i in range(num_colors):
-            binary_value = format(i, f'0{bits_needed}b')
-            color_to_binary[i] = binary_value
-        for y in range(height):
-            for x in range(width):
-                pixel = quantized_image.getpixel((x, y))
-                binary_code += color_to_binary[pixel]
-        logging.debug("Image converted to binary")
-        return binary_code
-    except Exception as e:
-        logging.error(f"Error in image_to_binary: {e}")
-        return ""
-
-def video_to_binary(video_path, num_colors=8, target_resolution=(640, 480)):
-    logging.debug(f"Processing video: {video_path}")
-    cap = cv2.VideoCapture(video_path)
-    binary_code = ""
-    frame_count = 0
-    if not cap.isOpened():
-        logging.error("Failed to open video file")
-        return ""
-
-    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    progress_data['total_frames'] = total_frames
-    logging.debug(f"Total frames to process: {total_frames}")
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-        frame = cv2.resize(frame, target_resolution)
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame_image = Image.fromarray(frame)
-        frame_binary = image_to_binary(frame_image, num_colors)
-        binary_code += frame_binary
-        frame_count += 1
-        progress_data['processed_frames'] = frame_count
-        time.sleep(0.1)  # Simulate processing time
-        logging.debug(f"Processed frame {frame_count} (Out of {total_frames})")
-    cap.release()
-    logging.debug(f"Video processed, total frames: {frame_count}")
-    return binary_code
+# List to store hashes
+hash_list = []
 
 def hash_binary_data(binary_data):
     try:
@@ -112,12 +63,12 @@ def search_metadata_for_hash(wallet_address, hash_value):
             break
     return None
 
-#def search_entire_blockchain_for_hash(hash_value):
+# def search_entire_blockchain_for_hash(hash_value):
 #   headers = {'project_id': API_KEY}
 #   page = 1
-#    while True:
-#        try:
-#            response = requests.get(f"{API_URL}/metadata/txs/labels?page={page}", headers=headers)
+#   while True:
+#       try:
+#           response = requests.get(f"{API_URL}/metadata/txs/labels?page={page}", headers=headers)
 #            response.raise_for_status()
 #            labels = response.json()
 #            if not labels:
@@ -152,63 +103,58 @@ def search_metadata_for_hash(wallet_address, hash_value):
 #            break
 #    return None
 
-def compare_videos(video1_path, video2_path, num_colors=8, target_resolution=(640, 480)):
-    logging.debug(f"Comparing videos: {video1_path} and {video2_path}")
-    cap1 = cv2.VideoCapture(video1_path)
-    cap2 = cv2.VideoCapture(video2_path)
+def video_to_binary(video_path, num_colors=8, target_resolution=(640, 480)):
+    logging.debug(f"Processing video: {video_path}")
+    cap = cv2.VideoCapture(video_path)
+    binary_code = ""
     frame_count = 0
-    differences = []
+    if not cap.isOpened():
+        logging.error("Failed to open video file")
+        return ""
 
-    if not cap1.isOpened() or not cap2.isOpened():
-        logging.error("Failed to open one or both video files")
-        return differences
-
-    total_frames1 = int(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
-    total_frames2 = int(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
-    total_frames = min(total_frames1, total_frames2)
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     progress_data['total_frames'] = total_frames
     logging.debug(f"Total frames to process: {total_frames}")
 
-    comparison_dir = "comparisons"
-    if not os.path.exists(comparison_dir):
-        os.makedirs(comparison_dir)
-
-    while cap1.isOpened() and cap2.isOpened():
-        ret1, frame1 = cap1.read()
-        ret2, frame2 = cap2.read()
-        if not ret1 or not ret2:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
             break
-
-        frame1 = cv2.resize(frame1, target_resolution)
-        frame2 = cv2.resize(frame2, target_resolution)
-        frame1_gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-        frame2_gray = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-
-        if not np.array_equal(frame1_gray, frame2_gray):
-            differences.append(frame_count)
-            diff_frame = cv2.absdiff(frame1, frame2)
-            _, diff_frame_thresh = cv2.threshold(diff_frame, 30, 255, cv2.THRESH_BINARY)
-            contours, _ = cv2.findContours(cv2.cvtColor(diff_frame_thresh, cv2.COLOR_BGR2GRAY), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            for contour in contours:
-                if cv2.contourArea(contour) > 50:  # Ignore small contours
-                    x, y, w, h = cv2.boundingRect(contour)
-                    cv2.rectangle(frame1, (x, y), (x + w, y + h), (0, 0, 255), 2)
-                    cv2.rectangle(frame2, (x, y), (x + w, y + h), (0, 0, 255), 2)
-
-            combined_frame = np.hstack((frame1, frame2))
-            comparison_image_path = os.path.join(comparison_dir, f"frame_{frame_count}.jpg")
-            cv2.imwrite(comparison_image_path, combined_frame)
-
+        frame = cv2.resize(frame, target_resolution)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_image = Image.fromarray(frame)
+        frame_binary = image_to_binary(frame_image, num_colors)
+        binary_code += frame_binary
         frame_count += 1
         progress_data['processed_frames'] = frame_count
+        logging.debug(f"Processed frame {frame_count}/{total_frames}")
         time.sleep(0.1)  # Simulate processing time
-        logging.debug(f"Processed frame {frame_count} (Out of {total_frames})")
 
-    cap1.release()
-    cap2.release()
-    logging.debug(f"Video comparison completed, total frames: {frame_count}")
-    return differences
+    cap.release()
+    logging.debug(f"Video processed, total frames: {frame_count}")
+    return binary_code
 
+def image_to_binary(image, num_colors=8):
+    try:
+        logging.debug("Converting image to binary")
+        quantized_image = image.convert("L").quantize(colors=num_colors)
+        width, height = quantized_image.size
+        binary_code = ""
+        palette = quantized_image.getpalette()
+        color_to_binary = {}
+        bits_needed = len(bin(num_colors - 1)[2:])
+        for i in range(num_colors):
+            binary_value = format(i, f'0{bits_needed}b')
+            color_to_binary[i] = binary_value
+        for y in range(height):
+            for x in range(width):
+                pixel = quantized_image.getpixel((x, y))
+                binary_code += color_to_binary[pixel]
+        logging.debug("Image converted to binary")
+        return binary_code
+    except Exception as e:
+        logging.error(f"Error in image_to_binary: {e}")
+        return ""
 
 @app.route('/upload_file', methods=['POST'])
 def upload_file():
@@ -220,7 +166,6 @@ def upload_file():
     logging.debug(f"Current hash: {current_hash}")
 
     if new_wallet and current_hash:
-        # Directly search the new wallet for the current hash
         logging.debug("Searching new wallet with provided hash")
         tx = search_metadata_for_hash(new_wallet, current_hash)
         if tx:
@@ -230,7 +175,7 @@ def upload_file():
                 'hash': current_hash,
                 'wallet': new_wallet,
                 'total_frames': progress_data.get('total_frames', 0),
-                'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                'processed_frames': progress_data.get('total_frames', 0)
             })
         else:
             logging.debug("No transaction found")
@@ -239,7 +184,7 @@ def upload_file():
                 'hash': current_hash,
                 'wallet': new_wallet,
                 'total_frames': progress_data.get('total_frames', 0),
-                'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                'processed_frames': progress_data.get('total_frames', 0)
             })
     elif file:
         file_path = os.path.join('uploads', file.filename)
@@ -275,7 +220,7 @@ def upload_file():
                         'hash': hash_result,
                         'wallet': wallet_to_use,
                         'total_frames': progress_data.get('total_frames', 0),
-                        'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                        'processed_frames': progress_data.get('total_frames', 0)
                     })
                 else:
                     logging.debug("No transaction found")
@@ -284,7 +229,7 @@ def upload_file():
                         'hash': hash_result,
                         'wallet': wallet_to_use,
                         'total_frames': progress_data.get('total_frames', 0),
-                        'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                        'processed_frames': progress_data.get('total_frames', 0)
                     })
             else:
                 logging.error("Failed to process file to binary")
@@ -310,14 +255,9 @@ def search_file():
         logging.debug(f"File saved to {file_path}")
 
         if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            try:
-                image = Image.open(file_path)
-                binary_code = image_to_binary(image)
-            except UnidentifiedImageError as e:
-                logging.error(f"Error opening image: {e}")
-                return jsonify({'message': 'Failed to process image: Unidentified image format.'})
+            binary_code = process_image(file_path)
         elif file.filename.lower().endswith('.mp4'):
-            binary_code = video_to_binary(file_path)
+            binary_code = "".join([frame.tobytes().decode('latin1') for frame in process_video(file_path)])
         else:
             logging.error("Unsupported file type")
             return jsonify({'message': 'Unsupported file type.'})
@@ -334,7 +274,7 @@ def search_file():
                     'hash': hash_result,
                     'wallet': tx[1].get('address', 'Unknown'),
                     'total_frames': progress_data.get('total_frames', 0),
-                    'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                    'processed_frames': progress_data.get('total_frames', 0)
                 })
             else:
                 logging.debug("No transaction found")
@@ -343,7 +283,7 @@ def search_file():
                     'hash': hash_result,
                     'wallet': '',
                     'total_frames': progress_data.get('total_frames', 0),
-                    'processed_frames': progress_data.get('total_frames', 0)  # Simulate 100% completion for videos
+                    'processed_frames': progress_data.get('total_frames', 0)
                 })
         else:
             logging.error("Failed to process file to binary")
@@ -370,17 +310,70 @@ def compare_videos_route():
         video2.save(video2_path)
         logging.debug(f"Videos saved to {video1_path} and {video2_path}")
 
-        differences = compare_videos(video1_path, video2_path)
+        cap1 = cv2.VideoCapture(video1_path)
+        cap2 = cv2.VideoCapture(video2_path)
+
+        frames1 = []
+        frames2 = []
+        while cap1.isOpened() or cap2.isOpened():
+            ret1, frame1 = cap1.read()
+            ret2, frame2 = cap2.read()
+            if ret1:
+                frames1.append(frame1)
+            if ret2:
+                frames2.append(frame2)
+            if not ret1 and not ret2:
+                break
+
+        cap1.release()
+        cap2.release()
+
         comparison_dir = "comparisons"
-        comparison_images = [f"/{comparison_dir}/frame_{frame}.jpg" for frame in differences]
+        if not os.path.exists(comparison_dir):
+            os.makedirs(comparison_dir)
 
-        if differences:
-            message = f"Differences found in frames: {differences}"
-        else:
-            message = "No differences found."
+        comparison_images = []
+        differences = []
+        max_len = max(len(frames1), len(frames2))
 
-        logging.debug(message)
-        return jsonify({'message': message, 'comparison_images': comparison_images})
+        for i in range(max_len):
+            if i < len(frames1) and i < len(frames2):
+                frame1 = frames1[i]
+                frame2 = frames2[i]
+            elif i < len(frames1):
+                frame1 = frames1[i]
+                frame2 = np.zeros_like(frame1)
+            else:
+                frame2 = frames2[i]
+                frame1 = np.zeros_like(frame2)
+
+            diff_img = cv2.absdiff(frame1, frame2)
+            gray_diff = cv2.cvtColor(diff_img, cv2.COLOR_BGR2GRAY)
+            _, threshold_diff = cv2.threshold(gray_diff, 30, 255, cv2.THRESH_BINARY)
+            contours, _ = cv2.findContours(threshold_diff, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+            total_diff = np.sum(threshold_diff) / 255
+            frame_area = frame1.shape[0] * frame1.shape[1]
+            percent_diff = (total_diff / frame_area) * 100
+            differences.append(percent_diff)
+
+            for contour in contours:
+                if cv2.contourArea(contour) > 500:  # Filter small differences
+                    x, y, w, h = cv2.boundingRect(contour)
+                    cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                    cv2.rectangle(frame2, (x, y), (x+w, y+h), (0, 0, 255), 2)
+                    explanation = f"Difference in region: (x: {x}, y: {y}, width: {w}, height: {h})"
+                    logging.debug(explanation)
+
+            frame1_path = os.path.join(comparison_dir, f"frame1_{i}.jpg")
+            frame2_path = os.path.join(comparison_dir, f"frame2_{i}.jpg")
+            cv2.imwrite(frame1_path, frame1)
+            cv2.imwrite(frame2_path, frame2)
+            comparison_images.append((frame1_path, frame2_path))
+
+        logging.debug(f"Differences: {differences}")
+
+        return jsonify({'message': 'Comparison complete', 'comparison_images': comparison_images, 'differences': differences})
     except Exception as e:
         logging.error(f"Error saving or processing videos: {e}")
         return jsonify({'message': 'Error processing videos.'})
@@ -393,17 +386,42 @@ def progress():
             yield f"data:{progress_data['processed_frames']}|{progress_data['total_frames']}\n\n"
     return Response(generate(), mimetype='text/event-stream')
 
-@app.route('/search_progress')
-def search_progress():
-    def generate():
-        while True:
-            time.sleep(1)
-            yield f"data:{progress_data['processed_frames']}|{progress_data['total_frames']}\n\n"
-    return Response(generate(), mimetype='text/event-stream')
-
 @app.route('/comparisons/<path:filename>')
 def serve_comparisons(filename):
     return send_from_directory('comparisons', filename)
+
+@app.route('/add_to_list', methods=['POST'])
+def add_to_list():
+    file = request.files.get('file')
+    if not file:
+        logging.error("No file uploaded")
+        return jsonify({'success': False, 'message': 'No file uploaded.'})
+
+    file_path = os.path.join('uploads', file.filename)
+    try:
+        file.save(file_path)
+        logging.debug(f"File saved to {file_path}")
+
+        if file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            binary_code = process_image(file_path)
+        elif file.filename.lower().endswith('.mp4'):
+            binary_code = "".join([frame.tobytes().decode('latin1') for frame in process_video(file_path)])
+        else:
+            logging.error("Unsupported file type")
+            return jsonify({'success': False, 'message': 'Unsupported file type.'})
+
+        if binary_code:
+            hash_result = hash_binary_data(binary_code)
+            logging.debug(f"Binary data hashed: {hash_result}")
+
+            hash_list.append({'file': file.filename, 'hash': hash_result})
+            return jsonify({'success': True, 'file': file.filename, 'hash': hash_result})
+        else:
+            logging.error("Failed to process file to binary")
+            return jsonify({'success': False, 'message': 'Failed to process file.'})
+    except Exception as e:
+        logging.error(f"Error saving or processing file: {e}")
+        return jsonify({'success': False, 'message': 'Error processing file.'})
 
 @app.route('/')
 def home():
